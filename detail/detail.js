5e0da0cca2dec78f8dd6e4588305bbb1a731c925
sirdan <stanford303@gmail.com>
2022-05-11 12:54:25 -0700

    added the required code and finished up

--- detail/detail.js
+++ detail/detail.js
@@ -2,7 +2,13 @@ import { getDog } from '../fetch-utils.js';
 import { renderDogDetail } from '../render-utils.js';
 
 const dogDetailContainer = document.getElementById('dog-detail-container');
-
+async function loadData() {
+    const par = new URLSearchParams(window.location.search);
+    const dog = await getDog(par.get('id'));
+    const doggie = renderDogDetail(dog);
+    dogDetailContainer.append(doggie);
+}
+loadData();
 // on load
 // get the id from URL
 // use the id to fetch the dog
--- fetch-utils.js
+++ fetch-utils.js
@@ -4,6 +4,9 @@ const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsI
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 export async function getDogs() {
+    const response = await client
+        .from('dogs')
+        .select();
     // from the dogs table, select all items
 
     // and return the response
@@ -11,6 +14,11 @@ export async function getDogs() {
 }
 
 export async function getDog(id) {
+    const response = await client
+        .from('dogs')
+        .select()
+        .match({ id: id })
+        .single();
     // from the dogs table, select a single dog who has the matching id
 
     // and return the response
